5b6a1fc43c24ad22a7ceb461696176e6201690f7
bd6981 <brittanyjdavis22@gmail.com>
2022-11-09 10:56:44 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Comp/Title';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Title/>
     </div>
   );
 }
---
+++ src/Comp/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Title = (props) => {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Title
